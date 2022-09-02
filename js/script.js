24b77f8827cc9d755f16575d051dbc575f5cf37c
QemerS <tu201806169@code.edu.az>
2022-09-02 16:44:45 +0400

    moving patterns from html to js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ function updateProgressBar() {
     progress.style.setProperty('--progress', scrollPercent);
 }
 // Proggress Bar code ends
+
+//regular expressions
+const reg_name = new RegExp('[A-Za-z]{2,}');
+const reg_email = new RegExp('[A-Za-z0-9\-\_\.]{1,}(@gmail.com)');
+const reg_address = new RegExp('[a-zA-Z]{1,}\/[a-zA-Z]{1,}\/[a-zA-Z]{1,}');
+const reg_postalCode = new RegExp('[0-9]{5}\-[0-9]]{4}|[0-9]{5}');
+const reg_ccNum = new RegExp('[0-9]{15,16}');
+const reg_ccType = new RegExp('(Visa)|(Mastercard)|(American Express)|(Discover)');
+const reg_cvv = new RegExp('[0-9]{3}');
